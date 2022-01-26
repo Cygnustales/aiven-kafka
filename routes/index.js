292645acbb4ac0b213148b1941c25c33045f5bb6
Budi Kusuma Utama <budi.kusuma@budis-MacBook-Pro.local>
2022-01-26 08:50:19 +0700

    route fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var kafka = require('../modules/kafka');
 
 router.post('/topic/data/', kafka.sendDataToTopics);
-router.post('/topic/describe/', kafka.describeTopicConfig);
+router.get('/topic/list/', kafka.topicList);
 router.post('/topic/message/', kafka.consumer);
-
+router.post('/topic/describe/', kafka.describeTopicConfig);
 module.exports = router;

ac4e0c2a04aeefdfc34c6cfa092beb14c119c6dc
Budi Kusuma Utama <budi.kusuma@budis-MacBook-Pro.local>
2022-01-26 08:43:05 +0700

    topic create delete

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var kafka = require('../modules/kafka');
 
-router.put('/topic/create/', kafka.createTopics);
 router.post('/topic/data/', kafka.sendDataToTopics);
 router.post('/topic/describe/', kafka.describeTopicConfig);
 router.post('/topic/message/', kafka.consumer);
